b27bb94f162ce61837fb39758cb8856d2877104b
vsvt2000 <viswajithvishnusai@gmail.com>
2022-08-13 13:08:03 +0530

    Basic CSS changes done

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div class="h-screen bg-slate-600">
+    
+    <div class="flex items-center justify-center h-screen bg-slate-600 text-center ">
+      <div >
       <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter the image ID here" required />
-      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " >Submit</button>
+      </div>
     </div>
+    
   )
 }

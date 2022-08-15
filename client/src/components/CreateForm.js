e03581ea3a2084aaa0201ea080aa7f970b221ea5
vsvt2000 <viswajithvishnusai@gmail.com>
2022-08-15 14:09:00 +0530

    Image preview basic change

--- client/src/components/CreateForm.js
+++ client/src/components/CreateForm.js
@@ -11,9 +11,12 @@ export default function CreateForm() {
     return (
         <div>
             <input type="file" name="image" onChange={(event) => {
-                setimage(event.target.files[0]);
+                
+                setimage(URL.createObjectURL(event.target.files[0]))
+                
                 console.log("Image selected");
             }} />
+            {(image===null)?null:<img src={image} alt="something"></img>}
             <button type="submit" id="submit" onClick={handleSubmit} value="Login" class="text-white bg-blue-700 m-1 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload</button>
         </div>
     );
